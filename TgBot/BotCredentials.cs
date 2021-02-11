04b3e9be86bd3c0c2a0bed2ee66615f02bcdf769
rePakkun <cj.ilya@yandex.ru>
2021-02-11 18:48:06 +0300

    rework

---
+++ TgBot/BotCredentials.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TgBot
+{  
+        public static class BotCredentials
+        {
+            public static readonly string BotToken = "1683927797:AAHMhzDomuAd6xT-2UYG53xRS7CJA1AZl8w";
+        }    
+}
--- TgBot/Program.cs
+++ TgBot/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot.Types.ReplyMarkups;
+using TelegramBot.Commands;
+using TgBot;
 
-namespace TgBot
+namespace TelegramBot
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var bot = new BotWorker();
+
+            bot.Inizalize();
+            bot.Start();
+
+            Console.WriteLine("Напишите stop для прекращения работы");
+
+            string command;
+            do
+            {
+                command = Console.ReadLine();
+
+            } while (command != "stop");
+
+            bot.Stop();
+
         }
+
+
     }
 }
